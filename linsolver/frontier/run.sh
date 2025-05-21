6778fe2438a6e2a836671d9cdc105704fb6ae52a
Weiqun Zhang <weiqunzhang@lbl.gov>
2025-05-21 12:17:28 -0400

    add linsolver/frontier

---
+++ linsolver/frontier/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+#SBATCH -J linsolver
+#SBATCH -A ast106
+#SBATCH -o %x-%j.out
+#SBATCH -t 00:30:00
+#SBATCH -p batch
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=7
+#SBATCH --gpus-per-task=1
+#SBATCH --gpu-bind=closest
+#SBATCH -N 4
+
+module load craype-accel-amd-gfx90a
+module load rocm
+module list
+
+export MPICH_GPU_SUPPORT_ENABLED=1
+
+srun -n 32 $(ls -tr main3d.*.MPI.HIP.ex | tail -n 1) inputs >& output.txt
+
+grep "Solve Time is " output.txt >& result.txt
